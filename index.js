99a5dbb90a290d57fd661a8ec53404cc13d37b39
Walker Le <walker.le88@gmail.com>
2022-12-28 14:57:00 -0700

    commit tree 4515765289e791fc39467798515733433b0808f5

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+
+// const drivers = ['D1', 'D2', 'D3']; // Example test array
+
+// const driverInfo = [
+//     {name: "name1", hometown: 'Denver'},
+//     {name: "name2", hometown: 'Dallas'},
+//     {name: "name3", hometown: 'Anaheim'}
+// ]
+
+function findMatching(array, string) {
+    return array.filter(element => element.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    return array.filter(element => element.substring(0, 1) === string.substring(0, 1));
+}
+
+function matchName(array, string) {
+    return array.filter(element => element.name === string)
+}
+
+// findMatching(drivers, "D1");
+// console.log(findMatching(drivers, "D1"));
+
+// fuzzyMatch(drivers, "D1");
+// console.log(fuzzyMatch(drivers, "D1"));
+
+// matchName(driverInfo, "name1");
+// console.log(matchName(driverInfo, "name1"));
